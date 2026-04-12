6780ffdfab3fc3195a34df57d148011b53eb33fd
leo <evanleo1021@gmail.com>
2026-04-12 20:33:16 +0900

    show banner image

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,10 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
 
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client = {queryClient}>
   <BrowserRouter>
-  
     <App />
   </BrowserRouter>
   </QueryClientProvider>
--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { usePopularMoviesQuery } from '../../../../hooks/usingPopularMovies';
+import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies';
 import Alert from 'react-bootstrap/Alert';
 import Spinner from 'react-bootstrap/Spinner';
 import "./Banner.style.css";
@@ -8,10 +8,10 @@ const Banner = () => {
   const {data,isLoading,isError,error} = usePopularMoviesQuery();
   console.log("ddd",data);
 
-  // 로딩 중일때
+  // 로딩 중일때 + 로딩스피너
   if (isLoading){
     return (
-      <div style={{ height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'black' }}>
+      <div className="banner loading-state">
         <Spinner animation="border" variant="danger" />
       </div>
     );
@@ -19,15 +19,14 @@ const Banner = () => {
 
   // 에러 발생시
   if(isError){
-    return <Alert variant="danger">{error.message}</Alert>;
+    return (<Alert variant="danger">{error.message}</Alert>);
   }
 
   // 데이터가 없다면 빈화면 return
   const movie = data?.results?.[0];
   if (!movie) {
-    return <div style={{ height: '600px', backgroundColor: 'black' }}></div>;
+    return null;
   }
-
   return (
     <div style ={{
       backgroundImage: `url(https://media.themoviedb.org/t/p/w1066_and_h600_face/${data.results[0]?.poster_path})`,
@@ -35,11 +34,12 @@ const Banner = () => {
     }}
     className="banner">
       
-      <div style={{ paddingLeft: '50px', backgroundColor: 'rgba(0,0,0,0.4)', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-        <h1 style={{ fontSize: '3.5rem', fontWeight: 'bold' }}>{movie.title}</h1>
-        <p style={{ maxWidth: '600px', fontSize: '1.2rem' }}>{movie.overview}</p>
+      <div className ="banner-text-area">
+        <h1>{movie.title}</h1>
+        <p>{movie.overview}</p>
+        <p className="release-date">Release Date : {movie.release_date}</p>
       </div>
-      </div>
+    </div>
   );
 };
 
