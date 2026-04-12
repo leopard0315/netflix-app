430113bb7aaae62c9336e05b2e0ff6a140821e8f
leo <evanleo1021@gmail.com>
2026-04-13 01:49:20 +0900

    add moviecard

---
+++ src/pages/Homepage/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Badge } from 'react-bootstrap';
+import "./MovieCard.style.css"
+
+const MovieCard = ({movie}) => {
+  return (
+    <div style={{backgroundImage : 
+    `url(https://media.themoviedb.org/t/p/w600_and_h900_face${movie.poster_path})`,
+    }}
+    className="movie-card"
+    >
+    <div className="overlay">
+      <h1>{movie.title}</h1>
+      {movie.genre_ids.map((id)=>(<Badge bg="danger">{id}</Badge>))}
+    </div>
+    <div>
+      <div>{movie.vote_average}</div>
+      <div>{movie.popularity}</div>
+      <div>{movie.adult ?`over18`:`under18`}</div>
+    </div>
+    </div>
+  )
+};
+
+export default MovieCard;
