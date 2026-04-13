b8096413d2e9be80724b58f56b83cb7d7022caf7
leo <evanleo1021@gmail.com>
2026-04-14 00:55:32 +0900

    modify: genre badge design

--- src/common/MovieCard/MovieCard.jsx
+++ src/common/MovieCard/MovieCard.jsx
@@ -1,23 +1,39 @@
 import React from 'react'
 import { Badge } from 'react-bootstrap';
 import "./MovieCard.style.css"
+import { useMovieGenreQuery } from '../../hooks/useMovieGenre';
 
 const MovieCard = ({movie}) => {
+
+  const {data:genreData} = useMovieGenreQuery()
+
+  const showGenre=(genreIdList)=>{
+    if(!genreData) return []
+    const genreNameList = genreIdList.map((id)=>{
+      const genreObj = genreData.find((genre)=>genre.id === id)
+      return genreObj.name;
+    })
+    return genreNameList;
+  }
+  
   return (
     <div style={{backgroundImage : 
     `url(https://media.themoviedb.org/t/p/w600_and_h900_face${movie.poster_path})`,
     }}
     className="movie-card"
     >
-    <div className="overlay">
-      <h1>{movie.title}</h1>
-      {movie.genre_ids.map((id)=>(<Badge bg="danger">{id}</Badge>))}
-    </div>
-    <div>
-      <div>{movie.vote_average}</div>
-      <div>{movie.popularity}</div>
-      <div>{movie.adult ?`over18`:`under18`}</div>
-    </div>
+      <div className="overlay">
+        <h2 className="movie-title">{movie.title}</h2>
+        <div className="attribute-group"> 
+          {showGenre(movie.genre_ids).map((id)=>(<Badge className="genre-badge" bg="danger">{id}</Badge>))}
+        </div>
+        
+        <div className="attribute-group info-row">
+          <Badge className="info-badge" bg="success" text="dark">⭐ {movie.vote_average.toFixed(1)}</Badge>
+          <Badge className="info-badge" bg="warning" text="dark"> {movie.adult ?`18+`:`ALL`}</Badge>
+        </div>
+        <div className="popularity-text">🔥{Math.floor(movie.popularity)}K</div>
+      </div>
     </div>
   )
 };
