b246b64ca41bc16dd21d93907440bb42b0836b74
leo <evanleo1021@gmail.com>
2026-04-15 01:45:13 +0900

    modify

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// 경로 2가지
+// nav바에서 클릭해서 온경우 -> popular movie 보여주기
+// keyword를 입력해서 온 경우 -> keyword와 관련된 영화들을 보여줌
 const MoviePage = () => {
   return (
     <div>MoviePage</div>
