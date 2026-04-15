1d8af9a1e52e1aaf5926f302d882052417fe6e9f
leo <evanleo1021@gmail.com>
2026-04-16 02:28:50 +0900

    add: keyword search

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,12 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSearchMovieQuery}  from '../../hooks/useSearchMovie';
+import { useSearchParams } from 'react-router-dom';
+import {Spinner} from 'react-bootstrap';
+import { Container,Row,Col,Alert} from 'react-bootstrap';
+import MovieCard from '../../common/MovieCard/MovieCard';
+import ReactPaginate from 'react-paginate';
 
 // 경로 2가지
-// nav바에서 클릭해서 온경우 -> popular movie 보여주기
-// keyword를 입력해서 온 경우 -> keyword와 관련된 영화들을 보여줌
+// 1) nav바에서 클릭해서 온경우 -> popular movie 보여주기
+// 2) keyword를 입력해서 온 경우 -> keyword와 관련된 영화들을 보여줌
+
+// pagination
+// 1) 설치
+// 2) page state 만들기
+// 3) pagination 클릭할때마다 page 바꿔주기
+// 4) page값이 바뀔때마다 useSearchMovie에 page까지 넣어서 fetch
 const MoviePage = () => {
+
+  const [query,setQuery] = useSearchParams();
+  const [page,setPage] = useState(0)
+  const keyword = query.get("q");
+
+  const {data,isLoading,isError,error} = useSearchMovieQuery({keyword});
+  const handlePageClick = ()=>{};
+
+  if(isLoading){
+        return (
+      <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <Spinner animation="border" variant="danger" />
+    </div>
+  );
+    };
+    if(isError){
+        return <Alert variant="danger">{error.message}</Alert>
+    };
+
   return (
-    <div>MoviePage</div>
+    <Container>
+      <Row>
+        <Col lg={4} xs = {12}>필터</Col>
+        <Col lg={8} xs = {12}>
+        <Row>
+          {data?.results.map((movie,index) =>(
+            <Col key = {index} lg={4} xs = {12}>
+              <MovieCard movie = {movie}/>
+            </Col>
+          ))}
+        </Row>
+        {/* <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={page}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      /> */}
+          
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
-export default MoviePage
+export default MoviePage;
